daa3e546b235b401ea7a23294fcb27148c3ab087
ericweidman <ericweidman@gmail.com>
2016-02-22 12:00:20 -0500

    Made login and home page.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,38 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
+    static String name;
 
     public static void main(String[] args) {
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if(name == null) {
+                        return new ModelAndView(m, "login.html");
+                    }else{
+                        m.put("name", name);
+                        return new ModelAndView(m, "home.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    name = request.queryParams("loginName");
+                    response.redirect("/");
+                    return "";
+                })
+        );
 
     }
 }
